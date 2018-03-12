09bdc4024912b679b84ef125f058b9c0b35a1663
dnp <doanhpham1995@gmail.com>
2018-03-11 23:25:17 -0400

    add table for past training component

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,20 +1,32 @@
 import { TrainingService } from './../training.service';
 import { Exercise } from './../exercice.model';
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) {}
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCancelExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
